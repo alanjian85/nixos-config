d69fbc64f71810113f2d6c3c486e8d7de83dcd59
Alan Jian <alanjian85@outlook.com>
2024-12-13 11:55:25 +0800

    feat: add home manager to Flake inputs

--- flake.nix
+++ flake.nix
@@ -3,12 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations.enigma = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./nixos/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.feedc0de = import ./home-manager/home.nix;
+        }
+      ];
     };
   };
 }
---
+++ home-manager/home.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "feedc0de";
+  home.homeDirectory = "/home/feedc0de";
+
+  home.stateVersion = "24.11";
+
+  programs.home-manager.enable = true;
+}
