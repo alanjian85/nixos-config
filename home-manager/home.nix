40d73b8bca05251aabd8babe89fdfcc9389a0623
Alan Jian <alanjian85@outlook.com>
2025-03-18 21:39:37 +0800

    feat: enable GnuPG and gpg-agent

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,6 +3,15 @@
   home.username = "feedc0de";
   home.homeDirectory = "/home/feedc0de";
 
+  home.packages = with pkgs; [
+    gnupg
+  ];
+
+  services.gpg-agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-curses;
+  };
+
   home.stateVersion = "24.11";
 
   programs.home-manager.enable = true;
